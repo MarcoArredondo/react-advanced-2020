77f85a08493216aa503b83977026ed574c7a8de4
Marco Arredondo <marcoarredondoz28@gmail.com>
2020-12-12 11:44:46 -0600

    1-error-example

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = "Random title";
+  const handleClick = () => {
+    //Example on how is not possible to change tha value of title inside the component
+    //without re-rednering the component.
+    title = "Hello People";
+    console.log(title);
+  };
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button className="btn" type="button" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
