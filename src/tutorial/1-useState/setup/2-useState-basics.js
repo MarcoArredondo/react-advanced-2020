4bdde1fa43367a6a323a411eae1e3462317c4529
Marco Arredondo <marcoarredondoz28@gmail.com>
2020-12-12 12:34:31 -0600

    useState hook basic functionallity

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+//useState is a function
+//React hooks start with the word 'use'
+//Hooks need to be invoke inside the function/component body
+//Hooks cannot be call conditionally
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  //console.log(useState('Hello World'));
+  //const value = useState(1)[0];
+  //const handler = useState(1)[1];
+  //This operations can be donce using array destructring as below
+  const [text, setText] = useState("Random Title");
+  const handleClick = () => {
+    if (text === "Random Title") {
+      setText("Hello World");
+    } else {
+      setText("Random Title");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
