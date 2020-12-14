84fcc8d0407f7d497e2f80910e22a55d0fb6e532
Marco Arredondo <marcoarredondoz28@gmail.com>
2020-12-13 18:16:54 -0600

    How to use useState hook with objects

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Marco",
+    age: 26,
+    message: "Random message",
+  });
+  const changeMessage = () => {
+    //Spread operator to copy the old values and then we decide which value we want to change
+    setPerson({ ...person, message: "Hello world" });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        Change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
