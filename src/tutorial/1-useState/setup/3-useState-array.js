860016a10ec4367ca8fc362f48ef145dc574959a
Marco Arredondo <marcoarredondoz28@gmail.com>
2020-12-13 18:04:14 -0600

    Hoe to use useState with an array

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  //Another way to use React hook useState using array destructuring
+  const [people, setPeople] = React.useState(data);
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  return (
+    <>
+      {people.map((person) => {
+        //Object destructuring
+        const { id, name } = person;
+        return (
+          <div id={id} className="item">
+            <h4>{name}</h4>
+            <button className="btn" onClick={() => removeItem(id)}>
+              Remove item
+            </button>
+          </div>
+        );
+      })}
+      <button className="btn" onClick={() => setPeople([])}>
+        Clear
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
