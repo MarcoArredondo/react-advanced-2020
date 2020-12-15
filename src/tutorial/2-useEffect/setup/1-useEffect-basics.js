145e7a6ea6fa90265c8c8fa47cfcf8eed3c6a4c4
Marco Arredondo <marcoarredondoz28@gmail.com>
2020-12-14 19:22:14 -0600

    useEffect hook second parameter example

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -6,11 +6,20 @@ const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
 
   //useEffect hook is used when we want to execute some work outside the component (data fetching, listening for events, etc..)
+  //The second parameter is an array of dependencies if it's blanck that means that useEffect only runs on the initial render
+  //This will run when value attribute is changed
   useEffect(() => {
     if (value > 0) {
       document.title = `New messages (${value})`;
     }
-  });
+  }, [value]);
+
+  //You can have as many useEffect as you want
+  useEffect(() => {
+    console.log(
+      "This only runs on the first render because the second parameter"
+    );
+  }, []);
   return (
     <>
       <h1>{value}</h1>
