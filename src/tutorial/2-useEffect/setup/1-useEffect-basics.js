6a1dc70fd259648e6650ba8257923c89e4a1d3cc
Marco Arredondo <marcoarredondoz28@gmail.com>
2020-12-14 19:14:20 -0600

    Basci use of useEffect hook

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,29 @@
-import React, { useState, useEffect } from 'react';
-// by default runs after every re-render
+import React, { useState, useEffect } from "react";
+// by default useEfect runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  //useEffect hook is used when we want to execute some work outside the component (data fetching, listening for events, etc..)
+  useEffect(() => {
+    if (value > 0) {
+      document.title = `New messages (${value})`;
+    }
+  });
+  return (
+    <>
+      <h1>{value}</h1>
+      <button
+        className="btn"
+        onClick={() => {
+          setValue(value + 1);
+        }}
+      >
+        Click me
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
