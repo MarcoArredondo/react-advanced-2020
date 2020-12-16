389fd9b51b6d0a0d405e0e0d05ef9f83c04abcf1
Marco Arredondo <marcoarredondoz28@gmail.com>
2020-12-15 23:34:15 -0600

    Conditional rendering example with response status verification from fetch request

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,51 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("Default user");
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        //Catching a 404 error
+        if (response.status >= 200 && response.status <= 299) {
+          return response.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(response.statusText);
+        }
+      })
+      .then((user) => {
+        //Onbject deconstructing
+        const { login } = user;
+        setUser(login);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsError(true);
+      });
+  }, []);
+  if (isLoading) {
+    return (
+      <div>
+        <h2>loading...</h2>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div>
+        <h2>Error...</h2>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 };
 
 export default MultipleReturns;
