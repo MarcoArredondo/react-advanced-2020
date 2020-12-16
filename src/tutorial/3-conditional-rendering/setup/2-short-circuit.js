b8f8943df8e57a8d3b1a52cc71f9b374786eceaf
Marco Arredondo <marcoarredondoz28@gmail.com>
2020-12-16 15:57:07 -0600

    Short-circuit evaluation and ternary operator example

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 
 const ShortCircuit = () => {
   const [text, setText] = useState("");
-
+  const [isError, setIsError] = useState(false);
   //const firstValue = text || "hello world";
   //const secondValue = text && "hello world";
 
@@ -12,6 +12,21 @@ const ShortCircuit = () => {
     <>
       <h1>{text || "Default value in case text is false"}</h1>
       <h1>{text && "This text will be displayed in case text is true"}</h1>
+      <hr />
+      <h1>{isError && <h1>Error..</h1>}</h1>
+      {isError ? (
+        <p>True case in ternay operator (There's an error)</p>
+      ) : (
+        <p>False case in ternary operator (There's no error)</p>
+      )}
+      <button
+        className="btn"
+        onClick={() => {
+          setIsError(!isError);
+        }}
+      >
+        Toggle Error
+      </button>
     </>
   );
 };
