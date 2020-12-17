942f710e9e9461162a6053308530d778ce5a0de4
Marco Arredondo <marcoarredondoz28@gmail.com>
2020-12-16 21:45:03 -0600

    Handling forms using state variables linked to form inputs

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,24 +8,61 @@ import React, { useState } from "react";
 const ControlledInputs = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Hello World");
+    if (firstName && email) {
+      //If the key name matches the variable we can ommit one step on ES6
+      const person = { id: new Date().getTime().toString(), firstName, email };
+      setPeople((people) => {
+        //Spread operator returning an array with the current state values and adding the new value
+        return [...people, person];
+      });
+      setFirstName("");
+      setEmail("");
+    }
   };
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
   return (
     <React.Fragment>
       <article>
         <form className="form" onSubmit={handleSubmit}>
           <div className="form-control">
             <label htmlFor="firstName">Name :</label>
-            <input type="text" id="firstName" name="firstName" />
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="email">Email :</label>
-            <input type="text" id="email" name="email" />
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
           </div>
           <button className="btn" type="submit">
             Add Person
           </button>
         </form>
+        {people.map((person, index) => {
+          const { id, firstName, email } = person;
+          return (
+            <div className="item" key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </article>
     </React.Fragment>
   );
