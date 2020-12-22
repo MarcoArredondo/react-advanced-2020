56999922a7b9d2de37d5dd3e722907820dde23a2
Marco Arredondo <marcoarredondoz28@gmail.com>
2020-12-22 16:36:42 -0600

    React router basics

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React from "react";
 // react router
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//Importing the ReactRouter and giving it some alias
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // pages
-import Home from './Home';
-import About from './About';
-import People from './People';
-import Error from './Error';
-import Person from './Person';
+//This are just components that we'll use as pages
+import Home from "./Home";
+import About from "./About";
+import People from "./People";
+import Error from "./Error";
+import Person from "./Person";
 // navbar
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  //Normally we wrap whole application inside the Router, in this case we have a separate file
+  return (
+    <Router>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/people">
+        <People />
+      </Route>
+    </Router>
+  );
 };
 
 export default ReactRouterSetup;
